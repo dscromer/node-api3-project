97b3d02d0bfcb9126d4e395364bf11810612e852
Dalton Cromer <cromer.dalton@gmail.com>
2020-08-13 18:54:07 -0400

    mvp and stretch complete

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
-const express = require('express');
+const express = require('express')
+const userRouter = require("./users/userRouter")
+const postRouter = require("./posts/postRouter")
 
 const server = express();
+const port = 8080
+
+server.use(express.json())
+
+server.use(userRouter)
+server.use(postRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +16,13 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const time = new Date().toISOString()
+  console.log(`${time} ${req.method} ${req.url}`)
+}
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`)
+})
 
 module.exports = server;
